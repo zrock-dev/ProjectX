c414c6c6f048faef19bc570f0ea8f18fac1320af
Santiago <santisaavcon@gmail.com>
2022-03-14 16:57:23 -0400

    Sala - Asientos Reservados

--- src/Boleto.java
+++ src/Boleto.java
@@ -2,7 +2,16 @@ public class Boleto {
     int precioTotal;
     int cantidadAsientos;
     int puntos;
+    Cliente cliente;
     Sala salaAsignada;
+
+    public Boleto(int precioTotal, int cantidadAsientos, int puntos, Cliente cliente, Sala salaAsignada) {
+        this.precioTotal = precioTotal;
+        this.cantidadAsientos = cantidadAsientos;
+        this.puntos = puntos;
+        this.cliente = cliente;
+        this.salaAsignada = salaAsignada;
+    }
 
     public void setPuntosCliente(Cliente cliente){
         cliente.sumarPuntos(puntos);
--- src/Sala.java
+++ src/Sala.java
@@ -3,13 +3,11 @@ import java.util.*;
 public class Sala {
     int capacidad;
     String codigoSala;
-    Map<Cliente, ArrayList<String>> butacasPorCliente;
-    Map<String, ArrayList<String>> butacas;
+    Map<String, Integer> butacas;
     String[] carteleraSala = new String[5];
 
     public Sala(String codigoSala){
         butacas = new Hashtable<>();
-        butacasPorCliente = new Hashtable<>();
         this.codigoSala = codigoSala;
     }
 
@@ -18,7 +16,7 @@ public class Sala {
     }
 
     public void setCapacidad(int capacidad) {
-        this.capacidad = capacidad;
+        this.capacidad = capacidad/10;
     }
 
     public void llenarCarteleraSala(){
@@ -28,43 +26,16 @@ public class Sala {
         }
     }
 
-    public void poblarButacas(){
-        int cantidadFilas = 10;
-        int columnas = capacidad/cantidadFilas;
-        char[] columnasCodigos = Utils.generarAbc(columnas);
-        for (int indexColumn = 0; indexColumn < columnas; indexColumn++) {
-            ArrayList<String> filaAsientos  = new ArrayList<>();
-            String columnaLetra = String.valueOf(columnasCodigos[indexColumn]);
-            for (int indexFila = 0; indexFila < cantidadFilas; indexFila++) {
-                filaAsientos.add(columnaLetra + (indexFila +1));
-            }
-            butacas.put(columnaLetra, filaAsientos);
+    public void generarButacas(){
+        for (char letra: Utils.generarAbc(capacidad)) {
+            butacas.put(String.valueOf(letra), 0);
         }
     }
 
-    public void showStatusAsientos(Cliente cliente){
-        System.out.println(butacasPorCliente.get(cliente));
-    }
-
-    public void reservarButacas(int cantidad, String codigoColumna, Cliente cliente){
-        ArrayList<String> asientosCliente = butacasPorCliente.get(cliente);
-        if (cantidad <= 10){
-            ArrayList<String> columna = butacas.get(codigoColumna);
-            for (int i = 0; i < cantidad; i++) {
-                asientosCliente.add(columna.remove(0));
-            }
-            butacasPorCliente.put(cliente, asientosCliente);
+    public void reservarButacas(String columna, int cantidadAsientosReservados){
+        int oldValue = butacas.get(columna);
+        if (cantidadAsientosReservados <= 10 && (oldValue + cantidadAsientosReservados) <= 10 ){
+            butacas.put(columna, oldValue + cantidadAsientosReservados);
         }
-    }
-
-    public static void main(String[] args) {
-        Sala sala = new Sala("A");
-        Cliente cliente = new Cliente(1312, "santi Ago", "col",
-                                "20/04/2003");
-        sala.setCapacidad(100);
-        sala.poblarButacas();
-        sala.reservarButacas(4, "B", cliente);
-        sala.reservarButacas(4, "A", cliente);
-        sala.showStatusAsientos(cliente);
     }
 }
